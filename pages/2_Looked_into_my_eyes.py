e63affa3011e42ccac739faf3e9cc72eb02e30da
Jayaditya Arora <138256492+HareKrishna4747@users.noreply.github.com>
2025-10-07 19:46:47 +0530

    Update 2_Looked_into_my_eyes.py

--- pages/2_Looked_into_my_eyes.py
+++ pages/2_Looked_into_my_eyes.py
@@ -1,7 +1,18 @@
 import streamlit as st
 
 st.set_page_config(page_title="The First Time You Looked Into My Eyes – 7th April 💕", layout="centered")
+st.markdown("""
+<style>
+/* Hide Streamlit top-right hamburger menu */
+#MainMenu {visibility: hidden;}
 
+/* Hide Streamlit footer */
+footer {visibility: hidden;}
+
+/* Hide sidebar if exists */
+[aria-label="Sidebar"] {display: none;}
+</style>
+""", unsafe_allow_html=True)
 # --- Custom Style ---
 st.markdown("""
 <style>
